cc230dc7022c06ed062d8f7c7f1735a0f538ea43
alana.rae.smith@gmail.com <alana.rae.smith@gmail.com>
2017-03-17 12:16:30 -0500

    Completed first program/exercise and included comments.

---
+++ helloWorld.js
@@ -0,0 +1,10 @@
+// First exercise in back-end 
+
+// How this works: Call the function below in the terminal through 
+// the command: node + name of JS file
+
+// In this case: node hello-world.js
+
+console.log('Hello World! Time to learn NodeJS!');
+
+// Run the command and you see the statement above in the terminal 
